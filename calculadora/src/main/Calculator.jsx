4a66b83927a9ac8818dacdd8963fcd15b94a6b67
Gustavo <gustavosm994@gmail.com>
2020-01-02 15:15:33 -0300

    Ajustando a lógica - falta ajusta com switch para não ficar com o eval

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -28,7 +28,27 @@ export default class Calculator extends Component {
     }
 
     setOperation(operation) {
+        if( this.state.current === 0) {
+            this.setState({operation, current: 1, clearDisplay: true})
+        } else {
+            const equals = operation === '='
+            const currentOperation = this.state.operation
 
+            const values = [...this.state.values]
+            try{
+            values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+            } catch(e){
+                values[0] = this.state.values[0]
+            }
+            values[1] = 0
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+        }
     }
 
     addDigit(n) {
@@ -40,6 +60,14 @@ export default class Calculator extends Component {
         const currentValue = clearDisplay ? '' : this.state.displayValue
         const displayValue = currentValue + n
         this.setState({ displayValue, clearDisplay: false })
+
+        if (n !== '.') {
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({ values })
+        }
     }
 
     render() {

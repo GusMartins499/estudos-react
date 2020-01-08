e69a5510d3b2ef8fdc23be7a8510e1799b654582
Gustavo <gustavosm994@gmail.com>
2020-01-08 12:25:17 -0300

    Adicionando footer

---
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props=>(
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2020
+            <a href='http://cubosis.com.br' target='_blank'> CuboSis</a>.
+        </strong>
+    </footer>
+)

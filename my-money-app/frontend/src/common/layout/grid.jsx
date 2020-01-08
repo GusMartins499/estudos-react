62152c02820b8cda800ac53c0a9284a92f6136ee
Gustavo <gustavosm994@gmail.com>
2020-01-08 13:23:11 -0300

    Integrando o layout grid no dashboard e criando um component row para alinhamento

--- my-money-app/frontend/src/common/layout/grid.jsx
+++ my-money-app/frontend/src/common/layout/grid.jsx
@@ -7,9 +7,9 @@ export default class Grid extends Component {
         let classes = ''
 
         if (cols[0]) classes += `col-xs-${cols[0]}`
-        if (cols[1]) clases += `col-sm-${cols[1]}`
-        if (cols[2]) clases += `col-sm-${cols[2]}`
-        if (cols[3]) clases += `col-sm-${cols[3]}`
+        if (cols[1]) classes += `col-sm-${cols[1]}`
+        if (cols[2]) classes += `col-sm-${cols[2]}`
+        if (cols[3]) classes += `col-sm-${cols[3]}`
 
         return classes
     }

59a5858c4fd2c2f5e33bf3b1d59717a430f1e9db
Gustavo <gustavosm994@gmail.com>
2020-01-14 01:03:58 -0300

    Backend finalizado

--- my-money-app/backend/src/api/billingCycle/billingCycle.js
+++ my-money-app/backend/src/api/billingCycle/billingCycle.js
@@ -2,25 +2,25 @@ const restful = require('node-restful')
 const mongoose = restful.mongoose
 
 const creditSchema = new mongoose.Schema({
-    name: { type: String, require: true },
-    value: { type: Number, min: 0, require: true }
+    name: { type: String, required: true },
+    value: { type: Number, min: 0, required: true }
 })
 
 const debtSchema = new mongoose.Schema({
-    name: { type: String, require: true },
-    value: { type: Number, min: 0, require: true },
+    name: { type: String, required: true },
+    value: { type: Number, min: 0, required: true },
     status: {
         type: String, require: false, uppercase: true,
         enum: ['PAGO', 'PENDENTE', 'AGENDADO']
     }
 })
 
-const billingCycle = new mongoose.Schema({
-    name: { type: String, require: true },
-    month: { type: Number, require: true, min: 1, max: 12 },
-    year: { type: Number, min: 1970, max: 2100, require: true },
+const billingCycleSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    month: { type: Number, min: 1, max: 12, required: true },
+    year: { type: Number, min: 1970, max: 2100, required: true },
     credits: [creditSchema],
-    debtSchema: [debtSchema]
+    debts: [debtSchema]
 })
 
 module.exports = restful.model('BillingCycle', billingCycleSchema)
--- my-money-app/backend/src/config/server.js
+++ my-money-app/backend/src/config/server.js
@@ -1,13 +1,17 @@
+const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
-
-const port = 3003
+const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
+server.use(queryParser())
+
 server.listen(port, function () {
-    console.log(`BACKEND IS RUNNING ON port ${port}`)
+    console.log(`BACKEND IS RUNNING ON PORT ${port}.`)
 })
 
 module.exports = server

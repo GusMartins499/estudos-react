db3174aa3be598134e99e49793772bb2ebc4ffb4
Gustavo <gustavosm994@gmail.com>
2020-01-16 00:29:33 -0300

    criação do redux da tab

--- my-money-app/frontend/src/main/reducers.js
+++ my-money-app/frontend/src/main/reducers.js
@@ -1,9 +1,12 @@
 import { combineReducers } from 'redux'
+
 import DashboardReducer from '../dashboard/dashboardReducer'
+import TabReducer from '../common/tab/tabReducer'
 
 const rootReducer = combineReducers({
     //dashboard: () => ({ summary: { credit: 100, debt: 50 } })
-    dashboard: DashboardReducer
+    dashboard: DashboardReducer,
+    tab: TabReducer
 })
 
 export default rootReducer

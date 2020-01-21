773c3624f034d7d1270b79ebf3ede536393c88c7
Gustavo <gustavosm994@gmail.com>
2020-01-21 14:22:52 -0300

    Criando o getList e o reducer

--- my-money-app/frontend/src/main/reducers.js
+++ my-money-app/frontend/src/main/reducers.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
+import BillingCycleReducer from '../billingCycle/billingCycleReducer'
 
 const rootReducer = combineReducers({
     //dashboard: () => ({ summary: { credit: 100, debt: 50 } })
     dashboard: DashboardReducer,
-    tab: TabReducer
+    tab: TabReducer,
+    billingCycle: BillingCycleReducer
 })
 
 export default rootReducer

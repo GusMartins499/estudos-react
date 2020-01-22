e5b53a0c5b41bf7f6a94333a8d40282b9d8a7d31
Gustavo <gustavosm994@gmail.com>
2020-01-21 22:09:57 -0300

    Exibindo mensagens de sucesso e erro #1 (está com erro)

--- my-money-app/frontend/src/billingCycle/billingCyclesActions.js
+++ my-money-app/frontend/src/billingCycle/billingCyclesActions.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { toastr } from 'react-redux-toastr'
 const BASE_URL = 'http://localhost:3003/api'
 
 export function getList() {
@@ -12,6 +13,12 @@ export function getList() {
 export function create(values) {
     //console.log(values)
     axios.post(`${BASE_URL}/billingCycles`, values)
+        .then(resp => {
+            toastr.success('Sucesso', 'Operação realizada com sucesso !')
+        })
+        /* .catch(e => {
+            e.response.data.forEach(error => toastr.error('Erro', error))
+        }) */
     return {
         type: 'TEMP'
     }

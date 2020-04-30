f3b6b2117afe2c116f893fb79e817df32ec87baf
Gustavo Martins <gustavosm994@gmail.com>
2020-04-30 16:06:04 -0300

    Finalizando backend

--- todo-app/backend/src/config/database.js
+++ todo-app/backend/src/config/database.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect('mongodb://localhost/todo')
+module.exports = mongoose.connect('mongodb://localhost/todo', 
+{ useMongoClient: true })
--- todo-app/backend/src/config/server.js
+++ todo-app/backend/src/config/server.js
@@ -2,9 +2,11 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express();
+const allowCors = require('./cors')
 
 server.use(bodyParser.urlencoded({ extended: true }))
-server.use(bodyParser.json())
+server.use(bodyParser.json({ extended: true }))
+server.use(allowCors)
 
 server.listen(port, function () {
     console.log(`BACKEND IS RUNNING ON PORT ${port}`)
